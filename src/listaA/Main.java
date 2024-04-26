74a3b624d74d29cd4c118da6e2d9619f9e1c6519
TiagoSM <tiagosantosmes@gmail.com>
2024-04-26 15:01:08 -0300

    Added loops for continue the code execution after the finished exercise

--- src/listaA/Main.java
+++ src/listaA/Main.java
@@ -7,18 +7,29 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		byte flag = 0;
 		byte opt;
+		
 		do {
-			System.out.println("Qual exercício você deseja fazer?");
-			opt = scanner.nextByte();
-			if(opt < 1 || opt > 4) {
-				System.out.println("Digite um número inteiro de 1 a 4!");
-				flag = 1;
-			} else {
-				flag = 0;
-			}
-		} while(flag == 1);
-		Exercicios exercise = new Exercicios(opt);
-		exercise.execute();
+			
+			do {
+				System.out.println("Qual exercício você deseja fazer?");
+				opt = scanner.nextByte();
+				if(opt < 1 || opt > 4) {
+					System.out.println("Digite um número inteiro de 1 a 4!");
+					flag = 1;
+				} else {
+					flag = 0;
+				}
+			} while(flag == 1);
+			
+			Exercicios exercise = new Exercicios(opt);
+			exercise.execute();
+			
+			do {
+				System.out.println("Digite 0 para sair, digite 1 para fazer mais um exercício: ");
+				flag = scanner.nextByte();
+			} while(flag < 0 || flag > 1);
+			
+		} while(flag == 0);
 		scanner.close();
 	}
 }
